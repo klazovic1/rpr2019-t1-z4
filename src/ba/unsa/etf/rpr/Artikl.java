13a7f39a2b7de896db97204fc8bfb3e9b56a4c07
Kemal Lazović <klazovic1@etf.unsa.ba>
2019-10-16 16:44:50 +0200

    Initial

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    String ime;
+    int cijena;
+    int kod;
+
+    Artikl (String s, int c, int k){
+
+        this.ime = s;
+        this.cijena = c;
+        this.kod = k;
+
+    }
+
+
+
+
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl[] artikli = new Artikl[50];
+
+
+
+
 }
